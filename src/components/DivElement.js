9b8c32519e446b42cbabf5dd1e7d46a6901bd977
Kateryna Masiuk <kmasiuk@uw.edu>
2021-04-12 10:30:43 -0700

    Week1 done

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,15 @@
 // Import HTMLElement here
+import HTMLElement from './HTMLElement';
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super('div', content);
+  }
+}
+
+const renderedDiv = new DivElement('What a wonderful world');
+console.log(renderedDiv.render());
 
 // Export class here
+export default DivElement;
--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,17 @@
 // Define class here
+class HTMLElement {
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
+
+  render() {
+    return `<${this.tag}>${this.content}</${this.tag}>`;
+  }
+}
+
+const renderedText = new HTMLElement('p', 'Hello');
+console.log(renderedText.render());
 
 // Export class here
-export default {};
+export default HTMLElement;
